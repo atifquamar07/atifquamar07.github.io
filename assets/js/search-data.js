78e307920c58316a216d4b269cf9929cd90e6641
atifquamar07 <88545624+atifquamar07@users.noreply.github.com>
2026-05-20 10:50:59 +0000

    Deploying to gh-pages from @ atifquamar07/atifquamar07.github.io@cff919edeeee2c53e966b7e7edcf3c4708284afb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "Publications from atifquamar.com.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
